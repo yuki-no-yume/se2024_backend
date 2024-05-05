f0f8b4c44edf7e79a92d880417d78bb6d4dd433b
Yuki <welt_reed@qq.com>
2024-05-06 02:33:31 +0800

    1.初步完成了对气温数据的插值算法

--- ylqk/api/meteorological_data.py
+++ ylqk/api/meteorological_data.py
@@ -1,11 +1,13 @@
+import numpy as np
 import requests
+import numpy as npy
+from scipy import interpolate
 from django.http import HttpRequest
 from django.views.decorators.http import require_GET, require_POST
 from datetime import datetime, timedelta
 
 from utils.response_util import *
 from ylqk.models.meteorological_data import *
-
 
 USER_ID = "714756904471BoKJR"
 PWD = "AhKNgEJ"
@@ -17,6 +19,9 @@ API = f"http://api.data.cma.cn:8090/api?userId={USER_ID}&pwd={PWD}" \
       "WIN_S_Avg_2mi,WIN_D_Avg_2mi,WIN_S_MAX,WIN_D_S_Max,WIN_S_Inst_Max,WIN_D_INST_Max," \
       "CLO_Cov,CLO_Cov_Low,CLO_COV_LM," \
       "VIS,WEP_Now"
+_lng_cn = npy.linspace(73.67, 135.08, 100)
+_lat_cn = npy.linspace(18.17, 53.5, 100)
+_lng_cn2d, _lat_cn2d = npy.meshgrid(_lng_cn, _lat_cn)
 
 
 def _list2str(lst: list) -> str:
@@ -33,12 +38,12 @@ def _list2str(lst: list) -> str:
 def _update_meteorological_data():
     # &timeRange=[20240404110000,20240411110000]&staIDs=54433,54399
     hours_delta = 3 if datetime.utcnow().hour % 3 == 0 else int(datetime.utcnow().hour % 3)
-    update_time = (datetime.utcnow()-timedelta(hours=hours_delta)).strftime("%Y%m%d%H0000")
+    update_time = (datetime.utcnow() - timedelta(hours=hours_delta)).strftime("%Y%m%d%H0000")
     basic_url = API + f"&timeRange=[{update_time},{update_time}]"
     stations = StationInfo.objects.all()
     index = 0
     while index < len(stations):
-        station_sub_list = stations[index:index+30]
+        station_sub_list = stations[index:index + 30]
         station_id_list = []
         for elm in station_sub_list:
             station_id_list.append(elm.station_id)
@@ -82,6 +87,14 @@ def _update_meteorological_data():
                 WEP_Now=elm["WEP_Now"]
             )
         index += 30
+
+
+def _interp2d(lng_list: list, lat_list: list, value_list: list):
+    # 建议对下列几种插值方法进行对比: inverse, linear
+    # 单从数值看,似乎inverse更优
+    f_interp = interpolate.Rbf(lng_list, lat_list, value_list, function="inverse")
+    interp_list = f_interp(_lng_cn2d, _lat_cn2d)
+    return interp_list
 
 
 @require_GET
@@ -153,3 +166,34 @@ def update_meteorological_data_by_admin(request: HttpRequest):
     # 此处加入管理员权限认证
     _update_meteorological_data()
     return build_success_json_response()
+
+
+@require_GET
+def get_interp_temperature_data(request: HttpRequest):
+    origin_temperature_data = TemperatureData.objects.all()
+    for elm in origin_temperature_data:
+        elm.station_info = StationInfo.objects.filter(station_id=elm.Station_Id_C).first()
+    longitude_list = []
+    latitude_list = []
+    temperature_list = []
+    for elm in origin_temperature_data:
+        if elm.TEM != 999999 and elm.TEM != 999998 and elm.TEM != 999990:
+            longitude_list.append(elm.station_info.longitude)
+            latitude_list.append(elm.station_info.latitude)
+            temperature_list.append(elm.TEM)
+    interp_values = _interp2d(longitude_list, latitude_list, temperature_list)
+    response = []
+    for i in range(0, len(_lng_cn2d)):
+        for j in range(0, len(_lat_cn2d)):
+            meta = {
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [_lng_cn2d[i][j], _lat_cn2d[i][j]]
+                },
+                "properties": {
+                    "count": interp_values[i][j],
+                }
+            }
+            response.append(meta)
+            # print(f"longitude = {_lng_cn2d[i][j]}, latitude = {_lat_cn2d[i][j]}, interp = {interp_values[i][j]}")
+    return build_success_json_response(response)
