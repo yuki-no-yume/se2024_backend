15f44f0c837cde5791b5f06641c2a36fe0d816c0
257949 <1055758552@qq.com>
2024-05-28 19:09:06 +0800

    发布灾害时访问接口补充经纬度

--- ylqk/api/sub_admin.py
+++ ylqk/api/sub_admin.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
 from django.views.decorators.http import require_GET
+import requests
 
 from utils.response_util import *
 from ..models.users import *
@@ -18,19 +19,35 @@ def admin_create_forewarn(request: HttpRequest):
         disaster.disaster_level = body.get("level")
     if body.get("location"):
         disaster.disaster_location = body.get("location")
-    if body.get("longitude"):
-        disaster.disaster_longitude = body.get("longitude")
-    if body.get("latitude"):
-        disaster.disaster_latitude = body.get("latitude")
+        locs = get_location_by_address(body.get("location"))
+        disaster.disaster_longitude = locs['lng']
+        disaster.disaster_latitude = locs['lat']
     if body.get("description"):
         disaster.disaster_description = body.get("description")
     disaster.save()
+
     # 给审核的发一条消息
     auditor = UserProfile.objects.filter(level='3').first()
 
-    mes = ForecastForAdmin.objects.create(status='1',send_id=aid,rec_id=auditor.id,disaster_id=disaster.id)
+    mes = ForecastForAdmin.objects.create(status='1', send_id=aid, rec_id=auditor.id, disaster_id=disaster.id)
     if body.get("remark"):
         mes.remark = body.get("remark")
     mes.save()
     resp_body = {"status_code": StatusCode.OK.value, "message": 'SUCCESS'}
     return HttpResponse(status=200, content=json.dumps(resp_body), content_type="application/json")
+
+
+def get_location_by_address(address):
+    result = LocationAndAddress.objects.filter(name=address).first()
+    if result:
+        return {"lng": result.longitude, "lat": result.latitude}
+    ak = 'riLHaKiDtfFXc8FU6qyzCMbjC2UDL9ga'
+    url = f"http://api.map.baidu.com/geocoding/v3/?address={address}&output=json&ak={ak}"
+    response = requests.get(url)
+    result = response.json()
+    if response.status_code == 200 and result.get('status') == 0:
+        location = result.get('result').get('location')
+        LocationAndAddress.objects.create(longitude=location['lng'],latitude=location['lat'],name=address)
+        return location  # 返回形如 {'lng': 116.404, 'lat': 39.915} 的字典
+    else:
+        return {"lng": -1, "lat": -1}
